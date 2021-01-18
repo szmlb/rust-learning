c475eee595d3ea84bc3f72ea745c12cc014d88b5
szmlb <szmlb.robotics@gmail.com>
2021-01-17 21:45:00 -0800

    Added code 4.7

---
+++ code4-7.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let mut f = Vec::with_capacity(50);
+    f.resize(50, 0);
+
+    f[0] = 0;
+    f[1] = 1;
+    for n in 2..50
+    {
+        f[n] = f[n-1] + f[n-2];
+        println!("{} term = {}", n, f[n]);
+    }
+
+}

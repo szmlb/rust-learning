a19e15547fdd02b613d18ef6e20e1879e3d82269
szmlb <szmlb.robotics@gmail.com>
2021-03-17 23:21:36 -0700

    Added code5-3.rs

---
+++ code5-3.rs
@@ -0,0 +1,46 @@
+mod common;
+
+fn chmin<T: Ord>(a: T, b: T) -> T{
+    if a > b
+    {
+        return b
+    }
+    else
+    {
+        return a
+    }
+}
+
+fn main() {
+
+    let INF = 1 << 60;
+
+    // Get input
+    println!("Type a number of data: ");
+    let n: usize = common::read_input();
+
+    println!("Type each data: ");
+    let mut h: Vec<i64> = Vec::with_capacity(n);
+    h.resize(n, 0);
+    for i in 0..n {h[i] = common::read_input();}
+
+    // Define array dp, initialized with inf
+    let mut dp: Vec<i64> = Vec::with_capacity(n);
+    dp.resize(n, INF);
+
+    // Initial condition
+    dp[0] = 0;
+
+    // loop
+    for i in 1..n 
+    {
+        dp[i] = chmin(dp[i], dp[i - 1] + (h[i] - h[i-1]).abs());
+        if i > 1 
+        {
+            dp[i] = chmin(dp[i], dp[i - 2] + (h[i] - h[i-2]).abs());
+        }
+    }
+        
+    println!("{}", dp[n-1]);
+
+}
